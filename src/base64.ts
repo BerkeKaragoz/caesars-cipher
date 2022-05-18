fc9e1ac2ce173bf49e1904c53bca6059417d1031
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2022-05-18 16:28:00 +0300

    refactor: rename exports

--- src/base64.ts
+++ src/base64.ts
@@ -7,8 +7,10 @@ const toBase64: Base64Converter = (string) =>
 const fromBase64: Base64Converter = (string) =>
   Buffer.from(string, "base64").toString();
 
-export const caesarsCipher: CaesarsCipher = (text, shiftAmount) =>
+export const base64CaesarsCipher: CaesarsCipher = (text, shiftAmount) =>
   _caesarsCipher(text, shiftAmount, toBase64);
 
-export const caesarsDecipher: CaesarsDecipher = (cipheredText, shiftAmount) =>
-  _caesarsDecipher(cipheredText, shiftAmount, fromBase64);
+export const base64CaesarsDecipher: CaesarsDecipher = (
+  cipheredText,
+  shiftAmount,
+) => _caesarsDecipher(cipheredText, shiftAmount, fromBase64);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
-export {
-  /** @alias browserCaesarsCipher */
-  caesarsCipher,
-  caesarsCipher as browserCaesarsCipher,
-  /** @alias browserCesarsDecipher */
-  caesarsDecipher,
-  caesarsDecipher as browserCesarsDecipher,
-} from "./uri-encoded";
+export { caesarsCipher, caesarsDecipher } from "./uri-encoded";
 
-export {
-  caesarsCipher as Base64CaesarsCipher,
-  caesarsDecipher as Base64CaesarsDecipher,
-} from "./base64";
+export { base64CaesarsCipher, base64CaesarsDecipher } from "./base64";
 
 export type { Base64Converter, CaesarsCipher, CaesarsDecipher } from "./types";
